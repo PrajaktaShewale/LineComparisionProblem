42f83eb84f86d69dd95c1f87e35d87432594eda9
PrajaktaShewale <shewalep94@gmail.com>
2022-10-28 08:18:17 +0530

    [Prajakta]Add:In Line Comparision Cal Length

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparision problem statement");
+            CalculateLength calculate = new CalculateLength(1, 2, 3, 4);
+            calculate.Calculate();
+        }
+    }
+}
---
+++ LineComparision/calculateLength.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LineComparison
+{
+    public class CalculateLength
+    {
+        double x1, x2, y1, y2;
+        public CalculateLength(double x1, double x2, double y1, double y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public void Calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+            Console.WriteLine("Length:-" + length);
+        }
+    }
+}
