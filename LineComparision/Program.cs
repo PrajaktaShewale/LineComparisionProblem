7ca98480d2204c5f8ea70353d8106334e70e64f5
PrajaktaShewale <shewalep94@gmail.com>
2022-10-28 08:56:07 +0530

    [Prajakta]Add:In Line Comparision two lines are eqaul or not

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -6,8 +6,14 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line comparision problem statement");
-            CalculateLength calculate = new CalculateLength(1, 2, 3, 4);
-            calculate.Calculate();
+            CalculateLength calculate1 = new CalculateLength(1, 2, 3, 4);
+            double length1 = calculate1.Calculate();
+            CalculateLength calculate2 = new CalculateLength(1, 2, 3, 4);
+            double length2 = calculate2.Calculate();
+            if (length1.Equals(length2))//length1 ==length2
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Both lines are not equal");
         }
     }
 }
--- LineComparision/calculateLength.cs
+++ LineComparision/calculateLength.cs
@@ -4,18 +4,19 @@ namespace LineComparison
 {
     public class CalculateLength
     {
-        double x1, x2, y1, y2;
-        public CalculateLength(double x1, double x2, double y1, double y2)
+        int x1, x2, y1, y2;
+        public CalculateLength(int x1, int x2, int y1, int y2)
         {
             this.x1 = x1;
             this.x2 = x2;
             this.y1 = y1;
             this.y2 = y2;
         }
-        public void Calculate()
+        public double Calculate()
         {
             double length = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
             Console.WriteLine("Length:-" + length);
+            return length;
         }
     }
 }
